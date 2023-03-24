e06bc797075274e3380195df8e4385e237bdcf11
zainhaditanoli <100013909+zainhaditanoli@users.noreply.github.com>
2023-03-24 09:34:01 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using practice.Creational_patterns;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s = Singleton.GetObject();
+            s.Print();
+            Singleton s2 = Singleton.GetObject();
+            s.Print();
+            Singleton s3 = Singleton.GetObject();
+            s.Print();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace practice.Creational_patterns
+{
+    class Singleton
+    {
+        public static Singleton a;
+        public static int objcount = 0;
+        private Singleton()
+        {
+        }
+        public static Singleton GetObject()
+        {
+            if (a == null)
+            {
+                a= new Singleton();
+                objcount += 1;
+            }
+            return a;
+        }
+        public void Print()
+        {
+            Console.WriteLine("object created the times:"+objcount);
+        }
+    }
+}
